95ef7a3052c168a96cf05c1519b3ae7896ee1749
jschreye <jschreye@c3r3s4.42lausanne.ch>
2022-07-14 10:26:10 +0200

    cube

---
+++ Makefile
@@ -0,0 +1,61 @@
+NAME = cub3D
+
+CC = gcc
+
+CFLAGS = -Wextra -Werror -Wall
+LDFLAGS = -Llibft -L$(HOME) -Lmlx -lmlx -framework OpenGL -framework Appkit
+
+AR = ar -rc
+
+SRCS_DIR = ./src
+OBJS_DIR = ./objs
+INC_DIRS = -Ilibft -I$(HOME)
+LIBFT_DIR = ./libft
+LIBFT = $(LIBFT_DIR)/libft.a
+MLX_DIR = ./mlx
+MLX = $(MLX_DIR)/libmlx.a
+
+SRCS =  main.c \
+
+OBJS = $(addprefix $(OBJS_DIR)/, $(notdir $(SRCS:.c=.o)))
+
+vpath %.c $(SRCS_DIR)
+
+RM = rm -f
+
+.PHONY: all re clean fclean debug
+
+all : $(NAME)
+
+$(NAME) : $(LIBFT) $(OBJS) $(MLX)
+	$(CC) -o $(NAME) $(OBJS) $(LIBFT) $(MLX) $(LDFLAGS)
+
+$(LIBFT) :
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(MLX) :
+	$(MAKE) -C $(MLX_DIR)
+
+$(OBJS_DIR):
+	mkdir -p $(OBJS_DIR)
+
+$(OBJS_DIR)/%.o : %.c | $(OBJS_DIR)
+	$(CC) $(CFLAGS) $(INC_DIRS) -o $@ -c $^
+
+debug: CFLAGS += -g3 -fsanitize=address -fno-omit-frame-pointer
+debug: LDFLAGS += -fsanitize=address
+debug: $(LIBFT) $(OBJS) $(NAME)
+
+valgrind: CFLAGS += -g3
+valgrind: $(LIBFT) $(OBJS) $(NAME)
+
+clean :
+	$(MAKE) -C $(LIBFT_DIR) clean
+	$(RM) -r $(OBJS_DIR)
+	$(MAKE) -C mlx clean
+
+fclean : clean
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	$(RM) $(NAME)
+
+re : fclean all
---
+++ main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschreye <jschreye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 10:45:29 by jschreye          #+#    #+#             */
+/*   Updated: 2022/07/11 13:34:44 by jschreye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "mlx/mlx.h"
+#include <stdlib.h>
+
+int	ft_check_key(int keycode, void *img, void *ptr, int x, int y)
+{
+    
+	if (keycode == 53)
+		exit(0);
+	if (keycode == 13 || keycode == 126)
+		mlx_pixel_put(img, ptr, x -= 5, y, 0xff00ff);
+	if (keycode == 1 || keycode == 125)
+		mlx_pixel_put(img, ptr, x += 5, y, 0xff00ff);
+	if (keycode == 2 || keycode == 124)
+		mlx_pixel_put(img, ptr, x, y -= 5, 0xff00ff);
+	if (keycode == 0 || keycode == 123)
+		mlx_pixel_put(img, ptr, x, y += 5, 0xff00ff);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+    void *img;
+    void *ptr;
+    int x = 500;
+    int y = 500;
+    
+    argc = 0;
+    argv = 0;
+    img = mlx_init();
+    ptr = mlx_new_window(img, 1000, 1000, "bouille");
+    mlx_pixel_put(img, ptr, x, y, 0xff00ff);
+    mlx_key_hook(ptr, ft_check_key, ptr);
+    mlx_loop(img);
+    return (0);
+}
